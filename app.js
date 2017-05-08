dacae1e1d763d3be639466e6079d5784366f6aac
dylanjshaw <dylanjshaw@sandiego.edu>
2017-05-08 11:19:26 -0700

    create a routing mechanism

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function(req, res){
-
-  res.writeHead(200, { 'Content-Type':'application/json'});
-  var data = {
-    firstname: 'Dylan',
-    lastname: 'Shaw'
-  };
-  res.end(JSON.stringify(data));
+  switch (req.url){
+    case '/': {
+      fs.createReadStream(__dirname + '/index.htm').pipe(res);
+      break;
+    }
+    case '/api': {
+      res.writeHead(200, { 'Content-Type':'application/json'});
+      var data = {
+        firstname: 'Dylan',
+        lastname: 'Shaw'
+      };
+      res.end(JSON.stringify(data));
+      break;
+    }
+    default: {
+      res.writeHead(404);
+      res.end();
+    }
+  }
 
   // var html = fs.createReadStream(__dirname + '/index.htm').pipe(res);
 
