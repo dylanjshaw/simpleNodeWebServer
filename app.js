1c71cb15d16de76100321412b569543c8ca32be5
dylanjshaw <dylanjshaw@sandiego.edu>
2017-05-08 10:49:31 -0700

    send data in json form

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
 
-  res.writeHead(200, { 'Content-Type':'text/html'});
-  var html = fs.createReadStream(__dirname + '/index.htm').pipe(res);
+  res.writeHead(200, { 'Content-Type':'application/json'});
+  var data = {
+    firstname: 'Dylan',
+    lastname: 'Shaw'
+  };
+  res.end(JSON.stringify(data));
+
+  // var html = fs.createReadStream(__dirname + '/index.htm').pipe(res);
 
 }).listen(1337, '127.0.0.1');
 //listen(port, localhost)
