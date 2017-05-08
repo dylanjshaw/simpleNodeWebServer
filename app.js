297a3c3f08c9398ba46ebfdeba177aaa024f5fb8
dylanjshaw <dylanjshaw@sandiego.edu>
2017-05-08 16:26:09 -0700

    Add middleware to link public files

--- app.js
+++ app.js
@@ -1,32 +1,52 @@
 // var moment = require('moment');
 // console.log(moment().format('ddd, hA'));
+// var http = require('http');
+// var fs = require('fs');
 
-var http = require('http');
-var fs = require('fs');
-var express = require('express')
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3000;
 
-http.createServer(function(req, res){
-  switch (req.url){
-    case '/': {
-      fs.createReadStream(__dirname + '/index.htm').pipe(res);
-      break;
-    }
-    case '/json': {
-      res.writeHead(200, { 'Content-Type':'application/json'});
-      var data = {
-        firstname: 'Dylan',
-        lastname: 'Shaw'
-      };
-      res.end(JSON.stringify(data));
-      break;
-    }
-    default: {
-      res.writeHead(404);
-      res.end();
-    }
-  }
+app.use('/assets', express.static(__dirname + '/public'));
+//this is middleware, fetching the stylesheet
 
-  // var html = fs.createReadStream(__dirname + '/index.htm').pipe(res);
+app.get('/', function(req, res){
+  res.send('<html><head><link href=assets/style.css type=text/css rel=stylesheet /></head><body><h1>Hello World!</h1></body></html>')
+});
 
-}).listen(1337, '127.0.0.1');
+app.get('/person/:name', function(req, res){
+  res.send(`<html><head></head><body><h1> What up, ${req.params.name}! </h1></body></html>`)
+});
+
+app.get('/api', function(req, res){
+  var obj = {key:'value'}
+  res.json(obj);
+})
+
+app.listen(port);
+
+// http.createServer(function(req, res){
+//   switch (req.url){
+//     case '/': {
+//       fs.createReadStream(__dirname + '/index.htm').pipe(res);
+//       break;
+//     }
+//     case '/json': {
+//       res.writeHead(200, { 'Content-Type':'application/json'});
+//       var data = {
+//         firstname: 'Dylan',
+//         lastname: 'Shaw'
+//       };
+//       res.end(JSON.stringify(data));
+//       break;
+//     }
+//     default: {
+//       res.writeHead(404);
+//       res.end();
+//     }
+//   }
+//
+//   // var html = fs.createReadStream(__dirname + '/index.htm').pipe(res);
+//
+// }).listen(1337, '127.0.0.1');
 //listen(port, localhost)
