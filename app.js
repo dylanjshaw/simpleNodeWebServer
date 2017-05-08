a8c3c92e69f560c143c21bb0cc9525ca1dba72d9
dylanjshaw <dylanjshaw@sandiego.edu>
2017-05-08 10:06:30 -0700

    respond with html file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var http = require('http');
-
+var fs = require('fs');
 
 http.createServer(function(req, res){
 
-  res.writeHead(200, { 'Content-Type':'text/plain'});
-  res.end('Hello World\n')
+  res.writeHead(200, { 'Content-Type':'text/html'});
+  var html = fs.readFileSync(__dirname + '/index.htm');
+  res.end(html);
 
 }).listen(1337, '127.0.0.1');
 //listen(port, localhost)
